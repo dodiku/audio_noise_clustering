960dbf7b12e66232f8d9664a205ec9785b31e82b
dodiku <dda290@nyu.edu>
2017-04-28 12:46:59 -0400

    init

---
+++ resources.py
@@ -0,0 +1,63 @@
+'''
+resources:
+    Comparing different clustering algorithms on toy datasets
+    http://scikit-learn.org/stable/auto_examples/cluster/plot_cluster_comparison.html#sphx-glr-auto-examples-cluster-plot-cluster-comparison-py
+
+    sklearn.cluster.SpectralClustering
+    http://scikit-learn.org/stable/modules/generated/sklearn.cluster.SpectralClustering.html
+
+    librosa.core.stft
+    https://librosa.github.io/librosa/generated/librosa.core.stft.html?highlight=stft#librosa.core.stft
+
+    matplotlib.mlab.specgram
+    http://matplotlib.org/api/mlab_api.html#matplotlib.mlab.specgram
+
+    Python find audio frequency and amplitude over time
+    http://stackoverflow.com/questions/6970943/python-find-audio-frequency-and-amplitude-over-time
+
+    spectrogram // mathworks
+    https://www.mathworks.com/help/signal/ref/spectrogram.html
+
+    Short Time Fourier Transform using Python and Numpy
+    https://kevinsprojects.wordpress.com/2014/12/13/short-time-fourier-transform-using-python-and-numpy/
+
+    numpy.fft.fft
+    https://docs.scipy.org/doc/numpy/reference/generated/numpy.fft.fft.html#numpy.fft.fft
+
+    nils-werner/stft
+    https://github.com/nils-werner/stft
+
+
+    more:
+    numpy.fft.fft
+    https://docs.scipy.org/doc/numpy/reference/generated/numpy.fft.fft.html
+
+    numpy.fft.rfft
+    https://docs.scipy.org/doc/numpy/reference/generated/numpy.fft.rfft.html
+
+    numpy.fft.fftfreq
+    https://docs.scipy.org/doc/numpy/reference/generated/numpy.fft.fftfreq.html
+
+
+    Color Palettes in Seaborn
+    https://chrisalbon.com/python/seaborn_color_palettes.html
+
+
+    py-webrtcvad
+    https://github.com/wiseman/py-webrtcvad
+    http://stackoverflow.com/questions/30409539/scipy-numpy-audio-classifier-voice-speech-activity-detection
+    https://pypi.python.org/pypi/webrtcvad
+
+    bob.bio.spear 3.0.1
+    https://pypi.python.org/pypi/bob.bio.spear
+
+    Single Speaker Word Recognition With Hidden Markov Models
+    https://kastnerkyle.github.io/posts/single-speaker-word-recognition-with-hidden-markov-models/
+
+    pydub
+    http://pydub.com/
+    https://github.com/jiaaro/pydub
+
+    Independent component analysis
+    https://en.wikipedia.org/wiki/Independent_component_analysis
+'''
